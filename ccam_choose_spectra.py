9168957676acb6283ace78c28530826ba36beac6
Ryan <rbanderson@usgs.gov>
2015-01-14 16:42:45 -0700

    ccam_choose_spectra

--- ccam_choose_spectra.py
+++ ccam_choose_spectra.py
@@ -4,25 +4,63 @@ Created on Wed Jan 14 14:12:02 2015
 
 @author: rbanderson
 
+This function allows spectra to be restricted to a specified composition range for a given element. 
+Optionally, a file can be provided specifying individual spectra that should be removed, regardless of composition
+
+Inputs:
+spectra = float array of spectra
+spect_index = int array of spectrum indices for each target
+comps = float array of target compositions
+compindex = the index of comps that corresponds to the element of interest
+mincomp = the lower limit of compositions to keep
+maxcomp = the upper limit of compositions to keep
+removelist = string specifying the path to an optional .csv file that lists individual spectra to remove.
+File should have two columns. The first column should have target names, the second column should have the index of the spectrum to remove.
+
+So, to remove the first and third spectrum of AGV2, the file would look like:
+
+AGV2,1
+AGV2,3
+
+Outputs:
+spectra_keep = array of spectra that satisfy the constraints on composition and are not listed in the file
+names_keep = names of spectra that satisfy the constraints on composition and are not listed in the file
+spect_index_keep = indices of spectra that satisfy the constraints on composition and are not listed in the file
+comps_keep = = compositions of spectra that satisfy the constraints on composition and are not listed in the file
+
 """
 import numpy
-def ccam_choose_spectra(spectra,spect_index,names,comps,mincomp,maxcomp,removelist=None):
-    #define new variable to hold the chosen data (don't want to overwrite the originals) 
-    spectra_keep=numpy.empty_like(spectra)
-    names_keep=numpy.empty_like(names)
-    spect_index_keep=numpy.empty_like(spect_index)
-    comps_keep=numpy.empty_like(comps)
+def ccam_choose_spectra(spectra,spect_index,names,comps,compindex,mincomp=0,maxcomp=100,removelist=None):
+
     
-    index=(comps>mincomp)&(comps<maxcomp) #define index where composition is within the specified range
-    
-    #fill the new variables with the data to keep
-    spectra_keep[index]=spectra[index]
-    names_keep[index]=names[index]
-    spect_index_keep[index]=spect_index[index]
-    comps_keep[index]=comps[index]
+    #define index where composition is within the specified range
+    index=(comps[:,compindex]>mincomp)&(comps[:,compindex]<maxcomp) 
     
     #optionally, remove spectra listed in an external file
     if removelist != None:
-        remove=numpy.genfromtxt(removelist,delimiter=',')
+        #read the list of sample names and spectrum indices from the file
+        removenames=numpy.genfromtxt(removelist,delimiter=',',usecols=0,dtype='string')
+        removeinds=numpy.genfromtxt(removelist,delimiter=',',usecols=1,dtype='int')
+        #define an array to hold the indices for each row in the file        
+        index2=numpy.empty([len(index),len(removenames)])
+        for i in range(len(removenames)):
+            #for each row, find the indices that correspond to the matching 
+            #name AND spectral index
+            index2[:,i]=(names==removenames[i])&(spect_index==removeinds[i])
+        #combine the indices for each row to a single array that indicates which
+        #spectra to remove, then invert it to indicate which spectra to keep
+        index2=numpy.invert(numpy.any(index2,axis=1))
+        #combine the indices from the file with the indices based on the composition range        
+        index=numpy.vstack((index,index2))
+        index=numpy.all(index,axis=0) #only keep spectra that satisfy composition range and are not in the list to remove
+        print index[0:10]
+    print index[0:10]    
+  
+    #fill the new variables with the data to keep
+    spectra_keep=spectra[index]
+    names_keep=names[index]
+    spect_index_keep=spect_index[index]
+    comps_keep=comps[index]
     
+    return spectra_keep,names_keep,spect_index_keep,comps_keep
     
